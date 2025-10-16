0762aa191e5fc819b0eaa7a289e17ffd7bac8bec
Aroobs-i <iftikhar8995@gmail.com>
2025-10-16 23:06:31 +0500

    hero

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+
+const Button = ({ title, id, rightIcon, leftIcon, ContainerClass}) => {
+  return (
+    <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${ContainerClass}`}>
+       {leftIcon}
+
+       <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
+        <div>
+          {title}
+        </div>
+       </span>
+       {rightIcon}
+    </button>
+  )
+}
+
+export default Button
