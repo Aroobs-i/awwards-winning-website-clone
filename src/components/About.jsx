b642a3d4136212d612cb2f5f9dd0d58b4d2e2499
Aroobs-i <iftikhar8995@gmail.com>
2025-10-17 11:58:36 +0500

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
         </div>
 
         <div className="h-dvh w-screen" id="clip">
-            <div className="mask-clip-path abou-image">
+            <div className="mask-clip-path about-image">
                 <img 
                  src="img/about.webp" 
                  alt="about"
